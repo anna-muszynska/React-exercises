9ec324be05b10dd04789174ea900fac8f19db5e9
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2021-12-14 20:18:49 +0100

    Working with portals

--- 04-mini-project/src/components/UI/ErrorModal.js
+++ 04-mini-project/src/components/UI/ErrorModal.js
@@ -1,23 +1,45 @@
+import ReactDOM from "react-dom";
+
 import classes from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onHandleError} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onHandleError}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
+
 function ErrorModal(props) {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onHandleError} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onHandleError}>Ok</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onHandleError={props.onHandleError} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onHandleError={props.onHandleError}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
 

a802fb5cf1672e32b0b151ccf490df0d7ece66f2
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-01 19:53:36 +0100

    Using async / await

--- 08-http-requests/src/App.js
+++ 08-http-requests/src/App.js
@@ -6,20 +6,19 @@ import './App.css';
 function App() {
     const [movies, setMovies] = useState([]);
 
-    function moviesHandler() {
-        fetch("https://swapi.dev/api/films/").then(response => {
-            return response.json();
-        }).then(data => {
-            const transformedMovies = data.results.map(movie => {
-                return {
-                    id: movie.episode_id,
-                    title: movie.title,
-                    openingText: movie.opening_crawl,
-                    releaseDate: movie.releseDate,
-                }
-            });
-            setMovies(transformedMovies);
+    async function moviesHandler() {
+        const response = await fetch("https://swapi.dev/api/films/");
+        const data = await response.json();
+
+        const transformedMovies = data.results.map(movie => {
+            return {
+                id: movie.episode_id,
+                title: movie.title,
+                openingText: movie.opening_crawl,
+                releaseDate: movie.releseDate,
+            };
         });
+        setMovies(transformedMovies);
     }
 
     return (

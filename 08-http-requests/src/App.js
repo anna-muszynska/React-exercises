7002c71a4535f4db4debdcdd6ad66fc494ebfb12
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-02 07:03:29 +0100

    Sending a POST request

--- 08-http-requests/src/App.js
+++ 08-http-requests/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -11,25 +12,30 @@ function App() {
     const moviesHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
-        
+
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            const response = await fetch(
+                'https://react-http-e6d4c-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
 
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
+            console.log(data);
 
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+
+            for (const key in data){
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                })
+            }
+
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -39,6 +45,18 @@ function App() {
     useEffect(() => {
         moviesHandler();
     }, [moviesHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch(
+            "https://react-http-e6d4c-default-rtdb.europe-west1.firebasedatabase.app/movies.json", {
+                method: "POST",
+                body: JSON.stringify(movie),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        const data = await response.json();
+    };
 
     let content = <p>Found no movies.</p>;
 
@@ -56,6 +74,9 @@ function App() {
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler}/>
+            </section>
             <section>
                 <button onClick={moviesHandler}>Fetch Movies</button>
             </section>

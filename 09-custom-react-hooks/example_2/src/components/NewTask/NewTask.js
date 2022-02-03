bab4a8e322b06eee288308167e68989367655768
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-03 19:44:35 +0100

    Using the custom hook in more components

--- 09-custom-react-hooks/example_2/src/components/NewTask/NewTask.js
+++ 09-custom-react-hooks/example_2/src/components/NewTask/NewTask.js
@@ -1,49 +1,33 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-e6d4c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+    const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    const createTask = (taskText, data) => {
+        const generatedId = data.name; // firebase-specific => "name" contains generated id
+        const createdTask = {id: generatedId, text: taskText};
 
-      const data = await response.json();
+        props.onAddTask(createdTask);
+    };
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+    const enterTaskHandler = async (taskText) => {
+        sendTaskRequest({
+            url: 'https://react-http-e6d4c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: {text: taskText}
+        }, createTask.bind(null, taskText))
+    };
 
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  return (
-    <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
-      {error && <p>{error}</p>}
-    </Section>
-  );
+    return (
+        <Section>
+            <TaskForm onEnterTask={enterTaskHandler} loading={isLoading}/>
+            {error && <p>{error}</p>}
+        </Section>
+    );
 };
 
 export default NewTask;

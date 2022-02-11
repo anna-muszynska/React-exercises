f16c60eec20f2edb5190cb5cb24971359b4732c4
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-11 19:16:28 +0100

    Dispatching actions with Redux Toolkit

--- 11-redux/src/components/Counter.js
+++ 11-redux/src/components/Counter.js
@@ -1,5 +1,6 @@
 import classes from './Counter.module.css';
 import {useSelector, useDispatch} from "react-redux";
+import {counterActions} from "../store/index";
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
@@ -7,19 +8,19 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-      dispatch({type: "INCREMENT"})
+      dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-      dispatch({type: "INCREASE", amount: 5})
+      dispatch(counterActions.increase(5)); // {type: SOME_UNIQUE_IDENTIFIER, payload: 5}
   };
 
   const decrementHandler = () => {
-      dispatch({type: "DECREMENT"})
+      dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-      dispatch({type: "TOGGLE"});
+      dispatch(counterActions.toggleCounter());
   };
 
   return (

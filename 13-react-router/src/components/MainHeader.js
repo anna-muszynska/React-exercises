98029240f537de8609be6f35152a63948a4eb2f5
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-21 07:45:20 +0100

    Using Routes and Links

---
+++ 13-react-router/src/components/MainHeader.js
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom";
+import styles from "./MainHeader.module.css";
+
+const MainHeader = () => {
+    return (
+        <header className={styles.header}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/welcome">Welcome</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainHeader;

ab534a95234ebbd470b81ebc448b0bba8a5f4ec5
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-02-23 07:53:31 +0100

    Switch, Route, Redirect, useParams, nested routes

---
+++ 13-react-router/example_2/src/pages/QuoteDetail.js
@@ -0,0 +1,18 @@
+import {Route, useParams} from "react-router-dom";
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail = () => {
+    const params = useParams();
+
+    return (
+        <section>
+            <h1>Quote Detail Page</h1>
+            <p>{params.quoteId}</p>
+            <Route path="/quotes/:quoteId/comments">
+                <Comments />
+            </Route>
+        </section>
+    )
+};
+
+export default QuoteDetail;

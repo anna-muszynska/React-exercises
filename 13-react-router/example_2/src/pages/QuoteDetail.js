c63bddc8d6e75d8df38d8c0718b9a75f2214a7ee
Anna Muszyńska <ania.maria.muszynska@gmail.com>
2022-03-17 22:32:53 +0100

    Sending and getting quote data via http

--- 13-react-router/example_2/src/pages/QuoteDetail.js
+++ 13-react-router/example_2/src/pages/QuoteDetail.js
@@ -1,32 +1,49 @@
 import {Link, Route, useParams, useRouteMatch} from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-    {id: 1, author: 'Ania', text: 'Learning React is fun!'},
-    {id: 2, author: 'Maria', text: 'Learning React is great!'},
-];
+import useHttp from "../hooks/use-http";
+import {getSingleQuote} from "../lib/api";
+import {useEffect} from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
     const params = useParams();
     const match = useRouteMatch();
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === Number(params.quoteId));
+    const {quoteId} = params;
 
-    if(!quote){
+    const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner/>
+            </div>
+        )
+    }
+
+    if (error) {
+        return <p className='centered'>{error}</p>
+    }
+
+    if (loadedQuote.text) {
         return <p>No quote found!</p>
     }
 
     return (
         <section>
-            <HighlightedQuote text={quote.text} author={quote.author}/>
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
             <Route path={match.path} exact>
                 <div className='centered'>
                     <Link className='btn--flat' to={`${match.url}/comments`}>Load comments</Link>
                 </div>
             </Route>
             <Route path={`${match.path}/comments`}>
-                <Comments />
+                <Comments/>
             </Route>
         </section>
     )
